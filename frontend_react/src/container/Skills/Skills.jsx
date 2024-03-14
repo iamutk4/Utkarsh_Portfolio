807d145a6dc9d6ea25bcf835b19a2ec3eaceeb52
iamutk4 <utkarsh_jadon@hotmail.com>
2024-03-14 02:18:47 -0400

    wrapped all containers inside MotionWrap | updated skill icons bg color

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 // import { Tooltip as ReactTooltip } from 'react-tooltip'
 import { Tooltip } from 'react-tooltip'
 
-import { AppWrap } from '../../wrapper';
+import { AppWrap, MotionWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
 import './Skills.scss';
 
@@ -94,4 +94,8 @@ const Skills = () => {
   )
 }
 
-export default AppWrap(Skills, 'skills');
+export default AppWrap(
+  MotionWrap(Skills, 'app__skills'), 
+  'skills',
+  'app__whitebg'
+);
