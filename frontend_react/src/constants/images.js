a8512f15ec64987f720bf44e972d4e88945eca99
iamutk4 <utkarsh_jadon@hotmail.com>
2024-04-11 00:18:26 -0400

    updated logos

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -43,6 +43,7 @@ import bolt from '../assets/bolt.png';
 import nb from '../assets/nb.png';
 import skype from '../assets/skype.png';
 import spotify from '../assets/spotify.png';
+import springboot from '../assets/springboot.png';
 
 import cv from '../assets/cv.png';
 
@@ -90,4 +91,5 @@ export default {
   email2,
   email3,
   cv,
+  springboot,
 };
